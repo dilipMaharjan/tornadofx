c490a291bf2ac8abd454ef2f41dad75c2aadd400
dilip <dilipmaharjann@gmail.com>
2018-05-23 09:00:13 -0400

    builder way

--- src/main/kotlin/com/learn/view/MainView.kt
+++ src/main/kotlin/com/learn/view/MainView.kt
@@ -1,18 +1,10 @@
 package com.learn.view
 
-import javafx.scene.layout.BorderPane
 import tornadofx.*
 
 class MainView : View() {
-    val topView: TopView by inject()
-    val bottomView: BottomView by inject()
-
-    override val root = BorderPane()
-
-    init {
-        with(root) {
-            top = topView.root
-            bottom = bottomView.root
-        }
+    override val root = borderpane {
+        top(TopView::class)
+        bottom(BottomView::class)
     }
 }
