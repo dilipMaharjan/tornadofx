92aade39147ad8a00d5ba4f0e11d0fe5310ddfdf
dilip <dilipmaharjann@gmail.com>
2018-05-23 09:14:25 -0400

    controller injection

---
+++ src/main/kotlin/com/learn/controller/MyController.kt
@@ -0,0 +1,13 @@
+package com.learn.controller
+
+import tornadofx.*
+
+
+/**
+ * Created by Dilip Maharjan on 05/23/2018
+ */
+class MyController : Controller() {
+    fun writeToDb(inputValue: String) {
+        println(inputValue)
+    }
+}
--- src/main/kotlin/com/learn/view/MainView.kt
+++ src/main/kotlin/com/learn/view/MainView.kt
@@ -1,12 +1,26 @@
 package com.learn.view
 
-import com.learn.app.Styles
+import com.learn.controller.MyController
+import javafx.scene.control.Alert
 import tornadofx.*
 
 class MainView : View("TornadoFX") {
-    override val root = hbox {
-        label(title) {
-            addClass(Styles.heading)
+    val myController: MyController by inject()
+
+    override val root = vbox {
+        label("Enter something") {
+            onHover {
+                alert(Alert.AlertType.INFORMATION, "Hover", "You hovered")
+            }
+        }
+
+
+        val inputField = textfield()
+        button("Ok") {
+            action {
+                myController.writeToDb(inputField.text)
+                inputField.clear()
+            }
         }
     }
 }
